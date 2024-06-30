066e31c4328166bd9a02a6da4d082f550fa60647
alex <12alexmaxu@gmail.com>
2024-06-30 22:30:00 +0200

    First Commit

---
+++ SoonToSpoon/SoonToSpoon/SoonToSpoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoonToSpoonApp.swift
+//  SoonToSpoon
+//
+//  Created by Alex  on 30/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SoonToSpoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
